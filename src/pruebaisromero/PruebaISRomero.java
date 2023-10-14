d6ed6863a28a4c2027254fc1841c3b0c2ce27eb7
romer <romer@LAPTOP-C4KLG9L5>
2023-10-13 22:55:21 -0500

    inicio de commit

---
+++ src/pruebaisromero/PruebaISRomero.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebaisromero;
+
+/**
+ *
+ * @author romer
+ */
+public class PruebaISRomero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("PRUEBA IS CHRISTIAN GITHUB");
+    }
+    
+}
